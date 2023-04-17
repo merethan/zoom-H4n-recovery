2e27280223b0737a767caa2ad87ab3b1cff11d0a
Maarten van Eeuwijk <chef@edgewanderer.com>
2023-04-17 20:30:10 +0200

    Implementing setting offset, which makes the intermediate step using 'dd' unneeded. Also some basic error checking and changes to output.

--- zoomdeinterleave.cpp
+++ zoomdeinterleave.cpp
@@ -3,8 +3,8 @@
 #include <string>
 #include <cstring> // memset()
 
-#define ZOOM_FAT32_SECTOR_SIZE 512 // In bytes
-#define ZOOM_FAT32_CLUSTER_SIZE 64 // In sectors
+#define ZOOM_FAT_SECTOR_SIZE 512 // In bytes
+#define ZOOM_FAT_CLUSTER_SIZE 64 // In sectors
 #define ZOOM_INTERLEAVE_SIZE 16 // In clusters
 #define ZOOM_INTERLEAVE_SIZE_DEVIATION 1 // Zoom H4n v1.90 alternates between writing 16 clusters of each file, writing 17 clusters of each etc.
 
@@ -14,44 +14,85 @@ int main(int argc, char *argv[])
 {
     string infile;
     string outfile[2];
+    uint32_t offset = 0;
 
     // Count of arguments is the program name itself, plus the arguments to it;
     // from argv[1] and onward is interesting to us
     switch(argc)
     {
-    case 2: // One argument in addition to the program name
+    case 2: // Program name, infile
         infile = argv[1];
-        outfile[0] = infile + ".m.wav";
-        outfile[1] = infile + ".i.wav";
+        outfile[0] = infile + ".M.wav"; // Using these letters sortof mimics the naming
+        outfile[1] = infile + ".I.wav"; // as used by the firmware itself
         break;
-    case 4: // Three arguments in addition to the program name
+    case 3: // Program name, offset, infile
+        offset = stol(argv[1]);
+        infile = argv[2];
+        outfile[0] = infile + "+offset.M.wav"; // Using these letters sortof mimics the naming
+        outfile[1] = infile + "+offset.I.wav"; // as used by the firmware itself
+        break;
+    case 4: // Program name, outfile, outfile, infile
         outfile[0] = argv[1];
         outfile[1] = argv[2];
         infile = argv[3];
         break;
+    case 5: // Program name, outfile, outfile, offset, infile
+        outfile[0] = argv[1];
+        outfile[1] = argv[2];
+        offset = stol(argv[3]);
+        infile = argv[4];
+        break;
     default:
-        cout << "Usage: [outfile_mics outfile_line] infile" << endl;
+        cout << "Usage: " << argv[0] << " [outfile_mics outfile_line] [offset] infile" << endl;
         return 1;
     }
 
-    cout << "Demuxing file " <<  infile << endl;
-    cout << "to files" << endl << outfile[0] << endl << outfile[1] << endl;
+    cout << "Deinterleaving file " << infile << " to files:" << endl << outfile[0] << endl << outfile[1] << endl;
+
+    // Given the Zoom H4n firmware always starts writing at the start of a FAT16/32 cluster, the offset should be
+    // a multiplum of the cluster size. The user may however not have used an image of a partition, the user could have
+    // made an image of the entire disk. In such case the disk label & MBR skew all cluster positions in the image
+    // and hence this assumption not applies. For this reason this program takes an offset in bytes, not in clusters.
+    // Do however warn the user in the case of an offset not aligning to cluster size
+    if(offset % (ZOOM_FAT_SECTOR_SIZE * ZOOM_FAT_CLUSTER_SIZE))
+    {
+        cout << "Warning: Supplied offset is not a multiplum of the cluster size used by the Zoom H4n firmware." << endl;
+        cout << "Data of an audio recording always starts at the beginning of a cluster!" << endl;
+        cout << "If you use this program on an image of a partition, you have made a calculation error." << endl;
+        cout << "Only when using this program on a full disk image can a valid offset be anything other than a multiplum of " << (ZOOM_FAT_SECTOR_SIZE * ZOOM_FAT_CLUSTER_SIZE) << endl;
+    }
 
     ifstream interleaved(infile);
+
+    if(!interleaved.is_open())
+    {
+        cout << "Failed to read input file " << infile << endl;
+        return 2;
+    }
+
     ofstream deinterleaved1(outfile[0], ios::out | ios::app | ios::binary); // Input-output stream,
     ofstream deinterleaved2(outfile[1], ios::out | ios::app | ios::binary); // out, append, binary
 
+    if(offset != 0)
+    {
+        if(!interleaved.seekg(offset, ios_base::beg)) // Set offset relative to begin of stream
+        {
+            cout << "Failed to seek to byte offset " << offset << ". Offset bigger than file?" << endl;
+            return 2;
+        }
+        cout << "Starting deinterleaving at cluster " << (offset / (ZOOM_FAT_SECTOR_SIZE * ZOOM_FAT_CLUSTER_SIZE)) << endl;
+    }
+
+    uint64_t bytecount = 0;
+
     for(uint32_t i = 0; !interleaved.eof(); i++)
     {
-        uint32_t chunksize = ZOOM_FAT32_SECTOR_SIZE * ZOOM_FAT32_CLUSTER_SIZE * ZOOM_INTERLEAVE_SIZE;
+        uint32_t chunksize = ZOOM_FAT_SECTOR_SIZE * ZOOM_FAT_CLUSTER_SIZE * ZOOM_INTERLEAVE_SIZE;
         if((i / 2) % 2 == 1)
-            chunksize += ZOOM_FAT32_SECTOR_SIZE * ZOOM_FAT32_CLUSTER_SIZE * ZOOM_INTERLEAVE_SIZE_DEVIATION;
+            chunksize += ZOOM_FAT_SECTOR_SIZE * ZOOM_FAT_CLUSTER_SIZE * ZOOM_INTERLEAVE_SIZE_DEVIATION;
 
         char buffer[chunksize];
         memset(buffer, 0, chunksize);
-
-        cout << "Writing chunk " << dec << noshowbase << i; // No endl
-        cout << " starting at offset " << hex << showbase << interleaved.tellg() << endl;
 
         interleaved.read(buffer, chunksize);
 
@@ -60,8 +101,10 @@ int main(int argc, char *argv[])
         else
             deinterleaved2.write(buffer, chunksize);
 
-        cout << "Deinterleaved " << dec << noshowbase << interleaved.gcount() << " bytes" << endl;
+        bytecount += chunksize;
     }
+
+    cout << "Deinterleaved " << dec << noshowbase << bytecount << " bytes (" << (bytecount / (ZOOM_FAT_SECTOR_SIZE * ZOOM_FAT_CLUSTER_SIZE)) << " clusters)" << endl;
 
     return 0;
 }
